64bd0ce559d6d27eb4801c377671de30922e5fca
Leonardo Carneiro <leonardo.muncone@agilize.com.br>
2024-03-20 22:57:09 -0300

    Finaliza CRUD

--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/service/CategoryService.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/category/service/CategoryService.java
@@ -1,13 +1,21 @@
 package br.com.cursoudemy.productapi.modules.category.service;
 
 import br.com.cursoudemy.productapi.config.exception.ValidationException;
+import br.com.cursoudemy.productapi.config.response.SuccessResponse;
 import br.com.cursoudemy.productapi.modules.category.dto.CategoryRequest;
 import br.com.cursoudemy.productapi.modules.category.dto.CategoryResponse;
 import br.com.cursoudemy.productapi.modules.category.model.Category;
 import br.com.cursoudemy.productapi.modules.category.repository.CategoryRepository;
+import br.com.cursoudemy.productapi.modules.product.service.ProductService;
+import br.com.cursoudemy.productapi.modules.supplier.dto.SupplierRequest;
+import br.com.cursoudemy.productapi.modules.supplier.dto.SupplierResponse;
 import br.com.cursoudemy.productapi.modules.supplier.model.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.aspectj.util.LangUtil.isEmpty;
 
@@ -16,11 +24,36 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductService productService;
+
+    public CategoryResponse findByIdResponse(Integer id) {
+        return CategoryResponse.of(findById(id));
+    }
 
     public Category findById(Integer id) {
+        validateInformedId(id);
         return categoryRepository
                 .findById(id)
                 .orElseThrow(() -> new ValidationException("There's no supplier for the given Id"));
+    }
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository
+                .findAll()
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<CategoryResponse> findByDescription(String description) {
+        if (isEmpty(description)) {
+            throw new ValidationException("The category description mus be informed");
+        }
+        return categoryRepository.findByDescription(description)
+                .stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
     }
 
     public CategoryResponse save(CategoryRequest request) {
@@ -28,9 +61,33 @@ public class CategoryService {
         return CategoryResponse.of(category);
     }
 
+    public CategoryResponse update(CategoryRequest request, Integer id) {
+        validateCategoryNameInformed(request);
+        validateInformedId(id);
+        var category = Category.of(request);
+        category.setId(id);
+        categoryRepository.save(category);
+        return CategoryResponse.of(category);
+    }
+
     private void validateCategoryNameInformed(CategoryRequest request) {
         if(isEmpty(request.getDescription())) {
             throw new ValidationException("The category description was not informed.");
+        }
+    }
+
+    public SuccessResponse delete(Integer id) {
+        validateInformedId(id);
+        if (productService.existsByCategoryId(id)) {
+            throw new ValidationException("You cannot delete this category because it's already defined by a product.");
+        }
+        categoryRepository.deleteById(id);
+        return SuccessResponse.create("The category was deleted.");
+    }
+
+    private void validateInformedId(Integer id) {
+        if (isEmpty(Collections.singleton(id))) {
+            throw new ValidationException("The category Id must be informed.");
         }
     }
 }
--- product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/service/ProductService.java
+++ product-api/src/main/java/br/com/cursoudemy/productapi/modules/product/service/ProductService.java
@@ -1,6 +1,7 @@
 package br.com.cursoudemy.productapi.modules.product.service;
 
 import br.com.cursoudemy.productapi.config.exception.ValidationException;
+import br.com.cursoudemy.productapi.config.response.SuccessResponse;
 import br.com.cursoudemy.productapi.modules.category.dto.CategoryRequest;
 import br.com.cursoudemy.productapi.modules.category.model.Category;
 import br.com.cursoudemy.productapi.modules.category.service.CategoryService;
@@ -8,9 +9,15 @@ import br.com.cursoudemy.productapi.modules.product.dto.ProductRequest;
 import br.com.cursoudemy.productapi.modules.product.dto.ProductResponse;
 import br.com.cursoudemy.productapi.modules.product.model.Product;
 import br.com.cursoudemy.productapi.modules.product.repository.ProductRepository;
+import br.com.cursoudemy.productapi.modules.supplier.dto.SupplierResponse;
+import br.com.cursoudemy.productapi.modules.supplier.model.Supplier;
 import br.com.cursoudemy.productapi.modules.supplier.service.SupplierService;
+import org.aspectj.util.LangUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -26,12 +33,72 @@ public class ProductService {
     @Autowired
     private CategoryService categoryService;
 
+    public List<ProductResponse> findAll() {
+        return productRepository
+                .findAll()
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public  Product findById(Integer id) {
+        return productRepository
+                .findById(id)
+                .orElseThrow(() -> new ValidationException("There's no product for the given Id."));
+    }
+
+    public ProductResponse findByIdResponse(Integer id) {
+        return ProductResponse.of(findById(id));
+    }
+
+    public List<ProductResponse> findByName(String name) {
+        return productRepository
+                .findByNameIgnoreCaseContaining(name)
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductResponse> findBySupplierId(Integer supplierId) {
+        if(isEmpty(supplierId)) {
+            throw new ValidationException("The product's supplier Id name must be informed.");
+        }
+        return productRepository
+                .findBySupplierId(supplierId)
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductResponse> findByCategoryId(Integer categoryId) {
+        if(isEmpty(categoryId)) {
+            throw new ValidationException("The product's category Id name must be informed.");
+        }
+        return productRepository
+                .findByCategoryId(categoryId)
+                .stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+
     public ProductResponse save(ProductRequest request) {
         validateProductDataInformed(request);
         validateCategoryAndSupplierInformed(request);
         var category = categoryService.findById(request.getCategoryId());
         var supplier = supplierService.findById(request.getSupplierId());
         var product = productRepository.save(Product.of(request, supplier, category));
+        return ProductResponse.of(product);
+    }
+
+    public ProductResponse update(ProductRequest request, Integer id) {
+        validateProductDataInformed(request);
+        validateInformedId(id);
+        validateCategoryAndSupplierInformed(request);
+        var category = categoryService.findById(request.getCategoryId());
+        var supplier = supplierService.findById(request.getSupplierId());
+        var product = Product.of(request, supplier, category);
+        product.setId(id);
+        productRepository.save(product);
         return ProductResponse.of(product);
     }
 
@@ -56,6 +123,27 @@ public class ProductService {
 
         if (isEmpty(request.getSupplierId())) {
             throw new ValidationException("The supplier Id was not informed.");
+        }
+    }
+
+    public Boolean existsByCategoryId(Integer categoryId) {
+        return productRepository.existsByCategoryId(categoryId);
+    }
+
+    public Boolean existsBySupplierId(Integer supplierId) {
+        return productRepository.existsBySupplierId(supplierId);
+    }
+
+    public SuccessResponse delete(Integer id) {
+        validateInformedId(id);
+        Product product = findById(id);
+        productRepository.delete(product);
+        return SuccessResponse.create("The product was deleted.");
+    }
+
+    public void validateInformedId(Integer id) {
+        if(isEmpty(id)) {
+            throw new ValidationException("The product Id must be informed");
         }
     }
 }
